eb3f14412f4806ddb041a1569476adafd33a3247
Mohummad-jubayer <61992910+Mohummad-jubayer@users.noreply.github.com>
2022-09-09 23:58:07 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// init Isotope
+var $grid = $(".project-lists").isotope({
+    // options
+});
+// filter items on button click
+$(".filter-button-group").on("click", "button", function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({
+        filter: filterValue
+    });
+});
+
+//Owl Carousel 2 function call
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    navText: ["PREV", "NEXT"],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+    },
+});
+
+// Wow js function call
+new WOW().init();
